422f4053c6ad64020c41cb7873e23a9ae4fb3774
hakanolgun <kaptanhakanolgun@gmail.com>
2021-07-16 14:15:39 +0300

    solve image problem by sumeyye's help

--- src/comps/Body.js
+++ src/comps/Body.js
@@ -5,6 +5,7 @@ function Body() {
   const API_key = "68fa173072924ae601704be2d93569fc";
   const { city, myWeather, setMyWeather } = useContext(MyContext);
 
+  //Bugün ve sonraki 7 günün tarihini bir array'e aldım
   let myDateArray = [];
   for (let day = 0; day < 8; day++) {
     let date = new Date();
@@ -12,38 +13,7 @@ function Body() {
     myDateArray.push(date);
   }
 
-  
-
-  const myImgFunc = () => {
-    let myimg = document.getElementsByClassName("myWeatherImg");
-    let returnvalue;
-    for (let i = 0; i < myimg.length; i++) {
-      switch (myimg[i].parentNode.previousSibling.textContent) {
-        case "Clear":
-          returnvalue = "http://openweathermap.org/img/wn/01d@2x.png";
-          break;
-        case "Rain":
-          returnvalue = "http://openweathermap.org/img/wn/10d@2x.png";
-          break;
-        case "Clouds":
-          returnvalue = "http://openweathermap.org/img/wn/02d@2x.png";
-          break;
-        case "Snow":
-          returnvalue = "http://openweathermap.org/img/wn/13d@2x.png";
-          break;
-        case "Drizzle":
-          returnvalue = "http://openweathermap.org/img/wn/09d@2x.png";
-          break;
-        case "Thunderstorm":
-          returnvalue = "http://openweathermap.org/img/wn/11d@2x.png";
-          break;
-        default:
-          returnvalue = "http://openweathermap.org/img/wn/01n@2x.png";
-      }
-      return returnvalue;
-    }
-  };
-
+  //Seçilen şehre göre ekran arkaplan resmini değiştirme
   let body = document.querySelector("body");
   switch (city.name) {
     case "Ankara":
@@ -63,40 +33,17 @@ function Body() {
       break;
     default:
       body.style.backgroundColor = "white";
-  };
+  }
 
-  // let Apps = document.getElementsByClassName("app");
-  // let myApp = Apps[0];
-  // switch (myWeather.cond0) {
-  //   case "Clear":
-  //     myApp.style.backgroundImage = `url("https://upload.wikimedia.org/wikipedia/commons/c/ce/An%C4%B1tkabir%2C_Ankara.jpg")`;
-  //     break;
-  //   case "Rain":
-  //     myApp.style.backgroundImage = `url("https://www.middleeastmonitor.com/wp-content/uploads/2020/07/20200710_2_43383664_56593866.jpg")`;
-  //     break;
-  //   case "Clouds":
-  //     myApp.style.backgroundImage = `url("https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Samsun-landing.jpg/1200px-Samsun-landing.jpg")`;
-  //     break;
-  //   case "Kocaeli":
-  //     myApp.style.backgroundImage = `url("https://upload.wikimedia.org/wikipedia/commons/1/1f/Kocaeli_Kartepe_Tarihi_Hikmetiye_Camii_DSC_6832.jpg")`;
-  //     break;
-  //   case "Rize":
-  //     myApp.style.backgroundImage = `url("https://images.unsplash.com/photo-1568960713091-53ccd34c0d7f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80")`;
-  //     break;
-  //   default:
-  //     myApp.style.backgroundColor = "white";
-  // };
-
-
-
-  useEffect( () => {
+  //City her değiştiğinde API'den yeni bilgileri isteme ve değişkenlere atama
+  useEffect(() => {
     const getWeather = async () => {
       const api_call = await fetch(
         `https://api.openweathermap.org/data/2.5/onecall?lat=${city.lat}&lon=${city.long}&units=metric&exclude=current,minutely,hourly,alerts&appid=${API_key}`
       );
-  
+
       const myresponse = await api_call.json();
-  
+
       setMyWeather((prev) => ({
         cond0: myresponse.daily[0].weather[0].main,
         min0: Math.floor(myresponse.daily[0].temp.min),
@@ -128,13 +75,39 @@ function Body() {
     myImgFunc();
   }, [city, setMyWeather]);
 
+  //Hava Durumuna göre uygun bulut resmini seçme
+  const myImgFunc = (myImg) => {
+      switch (myImg) {
+        case "Clear":
+          return "http://openweathermap.org/img/wn/01d@2x.png";
+          
+        case "Rain":
+          return "http://openweathermap.org/img/wn/09d@2x.png";
+          
+        case "Clouds":
+          return "http://openweathermap.org/img/wn/02d@2x.png";
+          
+        case "Snow":
+          return "http://openweathermap.org/img/wn/13d@2x.png";
+          
+        case "Drizzle":
+          return "http://openweathermap.org/img/wn/09d@2x.png";
+          
+        case "Thunderstorm":
+          return "http://openweathermap.org/img/wn/11d@2x.png";
+          
+        default:
+          return "http://openweathermap.org/img/wn/01n@2x.png";
+      }
+  };
+
   return (
     <div className="weathercardscontainer">
       <div className="dayCard">
         <p>Today</p>
         <p>{myWeather.cond0}</p>
         <p>
-          <img className="myWeatherImg" src={myImgFunc()} alt="" />
+          <img className="myWeatherImg" src={myImgFunc(myWeather.cond0)} alt="" />
         </p>
         <p className="tempParagrafi">
           <span>{myWeather.max0}&#176; </span>
@@ -145,7 +118,7 @@ function Body() {
         <p>{myDateArray[1]}</p>
         <p>{myWeather.cond1}</p>
         <p>
-          <img className="myWeatherImg" src={myImgFunc()} alt="" />
+          <img className="myWeatherImg" src={myImgFunc(myWeather.cond1)} alt="" />
         </p>
         <p className="tempParagrafi">
           <span>{myWeather.max1}&#176; </span>
@@ -156,7 +129,7 @@ function Body() {
         <p>{myDateArray[2]}</p>
         <p>{myWeather.cond2}</p>
         <p className="tempParagrafi">
-          <img className="myWeatherImg" src={myImgFunc()} alt="" />
+          <img className="myWeatherImg" src={myImgFunc(myWeather.cond2)} alt="" />
         </p>
         <p className="tempParagrafi">
           <span>{myWeather.max2}&#176; </span>
@@ -166,7 +139,7 @@ function Body() {
       <div className="dayCard">
         <p>{myDateArray[3]}</p>
         <p>{myWeather.cond3}</p>
-        <img className="myWeatherImg" src={myImgFunc()} alt="" />
+        <img className="myWeatherImg" src={myImgFunc(myWeather.cond3)} alt="" />
         <p className="tempParagrafi">
           <span>{myWeather.max3}&#176; </span>
           <span>{myWeather.min3}&#176; </span>
@@ -175,7 +148,7 @@ function Body() {
       <div className="dayCard">
         <p>{myDateArray[4]}</p>
         <p>{myWeather.cond4}</p>
-        <img className="myWeatherImg" src={myImgFunc()} alt="" />
+        <img className="myWeatherImg" src={myImgFunc(myWeather.cond4)} alt="" />
         <p className="tempParagrafi">
           <span>{myWeather.max4}&#176; </span>
           <span>{myWeather.min4}&#176; </span>
@@ -184,7 +157,7 @@ function Body() {
       <div className="dayCard">
         <p>{myDateArray[5]}</p>
         <p>{myWeather.cond5}</p>
-        <img className="myWeatherImg" src={myImgFunc()} alt="" />
+        <img className="myWeatherImg" src={myImgFunc(myWeather.cond5)} alt="" />
         <p className="tempParagrafi">
           <span>{myWeather.max5}&#176; </span>
           <span>{myWeather.min5}&#176; </span>
@@ -193,7 +166,7 @@ function Body() {
       <div className="dayCard">
         <p>{myDateArray[6]}</p>
         <p>{myWeather.cond6}</p>
-        <img className="myWeatherImg" src={myImgFunc()} alt="" />
+        <img className="myWeatherImg" src={myImgFunc(myWeather.cond6)} alt="" />
         <p className="tempParagrafi">
           <span>{myWeather.max6}&#176; </span>
           <span>{myWeather.min6}&#176; </span>
@@ -201,8 +174,8 @@ function Body() {
       </div>
       <div className="dayCard">
         <p>{myDateArray[7]}</p>
-        <p>{myWeather.cond6}</p>
-        <img className="myWeatherImg" src={myImgFunc()} alt="" />
+        <p>{myWeather.cond7}</p>
+        <img className="myWeatherImg" src={myImgFunc(myWeather.cond7)} alt="" />
         <p className="tempParagrafi">
           <span>{myWeather.max7}&#176; </span>
           <span>{myWeather.min7}&#176; </span>
